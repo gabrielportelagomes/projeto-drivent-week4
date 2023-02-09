4156b16afa7774aef2e6e6b3703de794343a7816
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-02-09 11:03:16 -0300

    refactor: removed minimum value

--- src/schemas/booking-schemas.ts
+++ src/schemas/booking-schemas.ts
@@ -2,5 +2,5 @@ import { CreateOrUpdateBooking } from "@/protocols";
 import Joi from "joi";
 
 export const createOrUpdateBookingSchema = Joi.object<CreateOrUpdateBooking>({
-  roomId: Joi.number().integer().min(1).required(),
+  roomId: Joi.number().integer().required(),
 });
